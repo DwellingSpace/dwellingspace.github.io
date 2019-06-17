f54737c52354d211b64610177faeb46750507c74
deploy.sh <>
2019-06-17 17:59:38 +0100

    Update eleventy config for CNAME file passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function(eleventyConfig) {
 
   // css, images and standalone scripts
-  eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("static");	
+
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   return {
     passthroughFileCopy: true
